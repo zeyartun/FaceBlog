f6d4d543408d618d6eeea75bd05c5063acb06bb8
zeyar <zeyarking125@gmail.com>
2020-09-15 22:57:36 +0630

    delete commit user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(CommentUserSeeder::class);
+        // $this->call(CommentUserSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(RoleUserSeeder::class);
